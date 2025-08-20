168a83375c6e74d577a8d910f2ea32f23ae10902
kayantang-ucb <kayan_tang@berkeley.edu>
2025-08-20 13:01:26 -0700

    Update newton.py

--- newton.py
+++ newton.py
@@ -5,10 +5,10 @@ def sec_der(f,x,e):
     return (first_der(f,x+e,e) - first_der(f,x,e)) / e
     
 def newton(f,x,e = 1e-4):
-    x1 = x
-    x0 = 0
-    while abs(x1-x0) > 1e-3:
-        x1 = x0 - first_der(f,x) / sec_der(f,x)
+    x0 = x
+    x1 = x0 - first_der(f,x0,e) / sec_der(f,x0,e)
+    while abs(x1 - x0) > 1e-3:
         x0 = x1
+        x1 = x0 - first_der(f,x0,e) / sec_der(f,x0,e)
     return x1
 
